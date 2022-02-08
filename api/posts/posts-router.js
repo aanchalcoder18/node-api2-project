55a40ad78767b4b221176a4b90aeaccec206042f
aanchalcoder18 <aasaxena14@gmail.com>
2022-02-08 13:29:04 -0800

    finished setup and task 1

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,21 @@
 // implement your posts router here
+const Post = require('./posts-model');
+const router = require('express').Router();
+
+
+/* #### 1 [GET] /api/posts */
+
+router.get('/', (req,res) =>{
+    Post.find(req.query)
+        .then(posts =>{
+            res.status(200).json(posts);
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+            });
+        });
+});
+
+module.exports = router;
